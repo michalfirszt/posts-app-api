87d764343f875cc1c6bf202123fd44fc8cef2520
michalfirszt <michalfiszu@gmail.com>
2020-11-04 20:15:41 +0100

    User profile

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -3,17 +3,17 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class ProfileFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Profile::class;
 
     /**
      * Define the model's default state.
@@ -23,11 +23,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->userName,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => bcrypt('password'),
-            'remember_token' => Str::random(10),
+            'user_id' => User::factory(),
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'description' => $this->faker->paragraph,
         ];
     }
 }
